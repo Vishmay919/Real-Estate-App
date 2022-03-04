d9078a23c08a893c87732e7bf176060ff3bb4fa8
Vishmay919 <95603986+Vishmay919@users.noreply.github.com>
2022-03-04 20:22:37 +0530

    Update [id].js

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -18,7 +18,7 @@ const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title
           AED {price}{rentFrequency? `/${rentFrequency}`: "/-"}
         </Text>
         <Spacer />
-        <Box border='1px' borderColor='gray.300' mx="5" px='6' rounded='full'>
+        <Box  mx="5" px='6' rounded='full'>
          By : {agency.name.substring(0,7)}...
         </Box>
       </Flex>
